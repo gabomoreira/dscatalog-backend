7acab9d9263d3944bc19f1627c1594dd7e750338
Gabriel Moreira <gsm.br2003@gmail.com>
2022-11-18 13:31:11 -0300

    backend project created

---
+++ src/main/java/com/gabosm/dscatalog/DscatalogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gabosm.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabosm/dscatalog/DscatalogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabosm.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
