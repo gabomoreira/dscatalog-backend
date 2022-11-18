e8371b6dfd6a9e4878f2f632a65a7eda40c085a3
Gabriel Moreira <gsm.br2003@gmail.com>
2022-11-18 15:17:24 -0300

    H2 database

--- src/main/java/com/gabosm/dscatalog/resources/CategoryResource.java
+++ src/main/java/com/gabosm/dscatalog/resources/CategoryResource.java
@@ -3,22 +3,25 @@ package com.gabosm.dscatalog.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabosm.dscatalog.entities.Category;
+import com.gabosm.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
+	
+	@Autowired
+	private CategoryService service;
 
 	@GetMapping()
 	public ResponseEntity<List<Category>> findAll() {
-		List<Category> categories = new ArrayList<Category>();
-		categories.add(new Category(1L, "Books"));
-		categories.add(new Category(2L, "Eletronics"));
+		List<Category> categories = service.findAll();
 		
 		return ResponseEntity.ok().body(categories);
 	}
